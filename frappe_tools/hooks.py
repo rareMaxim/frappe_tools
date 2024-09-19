ace91607343163c218b94abcdb037e3909648480
Maxim S <maks4a@gmail.com>
2024-09-19 17:08:30 +0300

    add: humans.txt. add: app swich

--- frappe_tools/hooks.py
+++ frappe_tools/hooks.py
@@ -4,12 +4,13 @@ app_publisher = "Maxim S"
 app_description = "Frappe Tools"
 app_email = "maks4a@gmail.com"
 app_license = "mit"
-app_logo_url = "/assets/asc/img/asc_mel.svg"
+app_logo_url = "/assets/frappe/images/frappe-framework-logo.svg"
 app_home = "/app/frappe-tools"
 
 
 website_route_rules = [
-    {"from_route": "/well-known/security.txt", "to_route": "security.txt"}
+    {"from_route": "/well-known/security.txt", "to_route": "security.txt"},
+    {"from_route": "/.well-known/security.txt", "to_route": "security.txt"},
 ]
 
 # Apps
@@ -248,4 +249,3 @@ export_python_type_annotations = True
 # default_log_clearing_doctypes = {
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
-
