fef6fbd561b2c53b7b6897e2d3d1f24a563ed9fc
Mykola Shevhenko <jonhsmithwich@gmail.com>
2024-11-16 15:22:05 +0200

    Code formatting

--- frappe_tools/frappe_tools/doctype/ft_document_export/ft_document_export.py
+++ frappe_tools/frappe_tools/doctype/ft_document_export/ft_document_export.py
@@ -9,64 +9,65 @@ from frappe.utils.data import cint
 
 
 class FTDocumentExport(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+    from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+    if TYPE_CHECKING:
+        from frappe.types import DF
 
-		doctype_type: DF.Link | None
-		export_type: DF.Literal["JSON"]
-	# end: auto-generated types
- 
-	def get_backup_name(self):
-		from datetime import datetime
-		now = datetime.now()
-		date_time = now.strftime("%Y-%d-%m %H:%M:%S")
-		return  f"{date_time} {self.doctype_type}.{self.export_type.lower()}"
+        doctype_type: DF.Link | None
+        export_type: DF.Literal["JSON"]
+    # end: auto-generated types
 
-	def get_all_data(self):
-		# Отримуємо список усіх DocType
-		doctypes = frappe.get_all(self.doctype_type, fields=["*"])
+    def get_backup_name(self):
+        from datetime import datetime
+        now = datetime.now()
+        date_time = now.strftime("%Y-%d-%m %H:%M:%S")
+        return f"{date_time} {self.doctype_type}.{self.export_type.lower()}"
 
-		# Збираємо дані про кожен DocType
-		doctype_data = {}
-		doctype_data.update({	"table": self.doctype_type,
-                       			"rows":doctypes
-                          })
+    def get_all_data(self):
+        # Отримуємо список усіх DocType
+        doctypes = frappe.get_all(self.doctype_type, fields=["*"])
 
-		# Зберігаємо зібрані дані у файл JSON
-		return frappe.as_json(doctype_data, ensure_ascii=False)
+        # Збираємо дані про кожен DocType
+        doctype_data = {}
+        doctype_data.update({"table": self.doctype_type,
+                             "rows": doctypes
+                             })
 
-	def upload_file(self, content, is_private=1):
-		"""
-		Завантажити файл у Frappe.
-		
-		:param file_path: Локальний шлях до файлу.
-		:param doctype: DocType, до якого прикріплюється файл (опціонально).
-		:param docname: Ім'я документа, до якого прикріплюється файл (опціонально).
-		:param is_private: Чи буде файл приватним (1 - приватний, 0 - публічний).
-		:return: Створений документ файлу.
-		"""
-		filename = self.get_backup_name()
-		# Створюємо документ File
-  
-		file_doc = frappe.get_doc(
-			{
-				"doctype": "File",
-				"attached_to_doctype": self.doctype,
-				"attached_to_name": self.name,
-				# "attached_to_field": fieldname,
-				"folder": "Home",
-				"file_name": filename,
-				# "file_url": file_url,
-				"is_private": cint(is_private),
-				"content": content,
-			}
-		).save(ignore_permissions=1)
-		return file_doc
+        # Зберігаємо зібрані дані у файл JSON
+        return frappe.as_json(doctype_data, ensure_ascii=False)
+
+    def upload_file(self, content, is_private=1):
+        """
+        Завантажити файл у Frappe.
+
+        :param file_path: Локальний шлях до файлу.
+        :param doctype: DocType, до якого прикріплюється файл (опціонально).
+        :param docname: Ім'я документа, до якого прикріплюється файл (опціонально).
+        :param is_private: Чи буде файл приватним (1 - приватний, 0 - публічний).
+        :return: Створений документ файлу.
+        """
+        filename = self.get_backup_name()
+        # Створюємо документ File
+
+        file_doc = frappe.get_doc(
+            {
+                "doctype": "File",
+                "attached_to_doctype": self.doctype,
+                "attached_to_name": self.name,
+                # "attached_to_field": fieldname,
+                "folder": "Home",
+                "file_name": filename,
+                # "file_url": file_url,
+                "is_private": cint(is_private),
+                "content": content,
+            }
+        ).save(ignore_permissions=1)
+        return file_doc
+
 
 @frappe.whitelist()
 def make_backup(docname):
--- frappe_tools/frappe_tools/doctype/ft_document_import/ft_document_import.py
+++ frappe_tools/frappe_tools/doctype/ft_document_import/ft_document_import.py
@@ -9,50 +9,53 @@ from frappe.model.document import Document
 
 
 class FTDocumentImport(Document):
-	# begin: auto-generated types
-	# This code is auto-generated. Do not modify anything in this block.
+    # begin: auto-generated types
+    # This code is auto-generated. Do not modify anything in this block.
 
-	from typing import TYPE_CHECKING
+    from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:
-		from frappe.types import DF
+    if TYPE_CHECKING:
+        from frappe.types import DF
 
-		count: DF.Int
-		doctype_type: DF.Link | None
-		file: DF.Attach | None
-	# end: auto-generated types
+        count: DF.Int
+        doctype_type: DF.Link | None
+        file: DF.Attach | None
+    # end: auto-generated types
 
-	def before_save(self):
-		if not self.file:
-			return
-		json = self.get_json_file()
-		self.count = len(json["rows"])
-		self.doctype_type = json["table"]
-	def get_title_field(self)->str:
-		meta = frappe.get_meta(self.doctype_type)
-		return meta.title_field or "name"
+    def before_save(self):
+        if not self.file:
+            return
+        json = self.get_json_file()
+        self.count = len(json["rows"])
+        self.doctype_type = json["table"]
 
-	def restore_backup(self):
-		data = self.get_json_file()["rows"]
-		for i, d in enumerate(data):
-			doc_data = {"doctype": self.doctype_type}
-			doc_data.update(d)
-			doc_title = doc_data[self.get_title_field()]
-			frappe.publish_progress(float(i) * 100 / self.count, title="Restore", description=doc_title)
-			doc = frappe.get_doc(doc_data)
-			try:
-				doc.insert(	set_name=doc_data["name"],
-               				ignore_links = True)
-			except:
-				pass
-		pass
+    def get_title_field(self) -> str:
+        meta = frappe.get_meta(self.doctype_type)
+        return meta.title_field or "name"
 
-	def get_json_file(self)->File:
-		file:File = frappe.get_last_doc("File", {"file_url": self.file})
-		return json.loads(file.get_content()) 
-	pass
+    def restore_backup(self):
+        data = self.get_json_file()["rows"]
+
+        for i, d in enumerate(data):
+            doc_data = {"doctype": self.doctype_type}
+            doc_data.update(d)
+            doc_title = doc_data[self.get_title_field()]
+            frappe.publish_progress(
+                float(i) * 100 / self.count, title="Restore", description=doc_title)
+            doc = frappe.get_doc(doc_data)
+            try:
+                doc.insert(set_name=doc_data["name"],
+                           ignore_links=True)
+            except:
+                pass
+
+    def get_json_file(self) -> File:
+        file: File = frappe.get_last_doc("File", {"file_url": self.file})
+        return json.loads(file.get_content())
+    pass
+
 
 @frappe.whitelist()
 def restore_backup(docname):
-	doc: FTDocumentImport = frappe.get_doc("FT Document Import", docname)
-	data = doc.restore_backup()
+    doc: FTDocumentImport = frappe.get_doc("FT Document Import", docname)
+    data = doc.restore_backup()
